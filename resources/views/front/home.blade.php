49ed5463565421ab8c660b986362533ec1fb3a23
omaisahmed <decentomais90@gmail.com>
2024-05-14 03:24:52 +0500

    show latest jobs in home

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -71,7 +71,56 @@
         </div>
     </section>
 
-    <section class="section-3  py-5">
+    <section class="section-3 bg-2 py-5">
+        <div class="container">
+            <h2>Latest Jobs</h2>
+            <div class="row pt-5">
+                <div class="job_listing_area">
+                <div class="job_lists">
+                    <div class="row">
+                        @if ($latestJobs->isNotEmpty())
+                            @foreach ($latestJobs as $latestJob)
+                            <div class="col-md-4">
+                                <div class="card border-0 p-3 shadow mb-4">
+                                    <div class="card-body">
+                                        <h3 class="border-0 fs-5 pb-2 mb-0">{{ $latestJob->title }}</h3>
+
+                                        <p>{{ Str::words(strip_tags($latestJob->description), 5) }}</p>
+
+                                        <div class="bg-light p-3 border">
+                                            <p class="mb-0">
+                                                <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
+                                                <span class="ps-1">{{ $latestJob->location }}</span>
+                                            </p>
+                                            <p class="mb-0">
+                                                <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
+                                                <span class="ps-1">{{ $latestJob->jobType->name }}</span>
+                                            </p>
+                                            @if (!is_null($latestJob->salary))
+                                            <p class="mb-0">
+                                                <span class="fw-bolder"><i class="fa fa-usd"></i></span>
+                                                <span class="ps-1">{{ $latestJob->salary }}</span>
+                                            </p>
+                                            @endif
+                                        </div>
+
+                                        <div class="d-grid mt-3">
+                                            <a href="{{ route('jobDetail',$latestJob->id) }}" class="btn btn-primary btn-lg">Details</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    {{-- <section class="section-3  py-5">
         <div class="container">
             <h2>Featured Jobs</h2>
             <div class="row pt-5">
@@ -118,52 +167,12 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <section class="section-3 bg-2 py-5">
         <div class="container">
             <h2>Latest Posts</h2>
             <div class="row pt-5">
-                {{-- <div class="job_listing_area">
-                <div class="job_lists">
-                    <div class="row">
-                        @if ($latestJobs->isNotEmpty())
-                            @foreach ($latestJobs as $latestJob)
-                            <div class="col-md-4">
-                                <div class="card border-0 p-3 shadow mb-4">
-                                    <div class="card-body">
-                                        <h3 class="border-0 fs-5 pb-2 mb-0">{{ $latestJob->title }}</h3>
-
-                                        <p>{{ Str::words(strip_tags($latestJob->description), 5) }}</p>
-
-                                        <div class="bg-light p-3 border">
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                                <span class="ps-1">{{ $latestJob->location }}</span>
-                                            </p>
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                                <span class="ps-1">{{ $latestJob->jobType->name }}</span>
-                                            </p>
-                                            @if (!is_null($latestJob->salary))
-                                            <p class="mb-0">
-                                                <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                                <span class="ps-1">{{ $latestJob->salary }}</span>
-                                            </p>
-                                            @endif
-                                        </div>
-
-                                        <div class="d-grid mt-3">
-                                            <a href="{{ route('jobDetail',$latestJob->id) }}" class="btn btn-primary btn-lg">Details</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-                        @endif
-                    </div>
-                </div>
-                </div> --}}
 
                 @if ($latestPosts->isNotEmpty())
                     @foreach ($latestPosts as $latestPost)
